6f196f3fec5015a0d2718b0db5e266a9a5ea00a8
William Dunston <wedunsto@aggies.ncat.edu>
2023-05-28 15:07:05 -0400

    Progressing adding Spring dependencies to a Gradle project

---
+++ Gradle-Demo/app/src/main/java/gradle/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package gradle.demo;
+
+public class HelloWorld {
+   private String message;
+
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+}
